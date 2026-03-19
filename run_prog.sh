f156f7eeefcaa52ec1034275508a1230874b5609
Jakob Drusany <drusany.jakob@gmail.com>
2026-03-19 19:44:42 +0100

    Add slurm shit

---
+++ run_prog.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# USAGE: ./run_prog fastcarve.cpp [ARGS]
+
+cat <<EOT
+
+#SBATCH --reservation=fri
+#SBATCH --job-name=code_sample
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --output=sample_out.log
+#SBATCH --hint=nomultithread
+
+# Set OpenMP environment variables for thread placement and binding    
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# Load the numactl module to enable numa library linking
+module load numactl
+
+# Compile
+gcc -O3 -lm -lnuma --openmp $1 -o a.out
+
+# Run
+srun a.out ${@:2}
+
+exit 0
+EOT
